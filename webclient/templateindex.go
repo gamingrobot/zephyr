ff3125bb38b8640cfba76cba643a7c8a602633f6
Morgan Creekmore <gamingrobot@gmail.com>
2014-03-18 17:11:39 -0600

    Added default avatar check

--- webclient/templateindex.go
+++ webclient/templateindex.go
@@ -2,6 +2,7 @@ package webclient
 
 import (
 	"github.com/codegangsta/martini-contrib/render"
+	. "github.com/gamingrobot/steamgo/internal"
 )
 
 type indexData struct {
@@ -13,19 +14,23 @@ type Friend struct {
 	Name       string
 	AvatarRoot string
 	Avatar     string
-	State      string
+	State      EPersonaState
 }
 
 func (w *WebHandler) templateIndex(r render.Render) {
 	index := indexData{}
 	steam := w.client.SteamHandler.steam
 	for _, f := range steam.Social.Friends.GetCopy() {
+		avatar := f.Avatar
+		if len(avatar) == 0 || avatar == "0000000000000000000000000000000000000000" {
+			avatar = "fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb" //Default ? avatar
+		}
 		friend := Friend{
 			SteamId:    f.SteamId.ToUint64(),
 			Name:       f.Name,
-			AvatarRoot: f.Avatar[0:2],
-			Avatar:     f.Avatar,
-			State:      string(f.PersonaState),
+			AvatarRoot: avatar[0:2],
+			Avatar:     avatar,
+			State:      f.PersonaState,
 		}
 		index.Friends = append(index.Friends, friend)
 	}
